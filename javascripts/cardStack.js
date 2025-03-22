fd8651cf85d0db89bcdecf8d1aba024d507960a4
Kong <bodunduoutlook.com>
2025-03-22 19:09:34 +0100

    test

--- javascripts/cardStack.js
+++ javascripts/cardStack.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
         expandCards();
         setTimeout(() => {
           adjustStackAlignContent(stack);
-        }, 200); // 延迟100 毫秒执行
+        }, 400); // 延迟100 毫秒执行
       } else {
         collapseCards();
       }
